e9b9674b91172c4ac802a5fc058ef01b8cc9a2f2
shware10 <112537781+shware10@users.noreply.github.com>
2024-05-13 19:12:17 +0900

    Revert "게임 오버 시 플레이팹 업데이트 기능 구현"
    This reverts commit 2acb1dd9a9ad84145541422b9ead3580310e49d4.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-using Photon.Realtime;
+
 public class Player : MonoBehaviour
 {
     [SerializeField]
     Transform[] points;
-    [SerializeField] UIManager uiManager;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
     }
 
     // Update is called once per frame
@@ -25,9 +22,6 @@ public class Player : MonoBehaviour
 
     void Go2Map()
     {
-        uiManager.isGameStart = true;
-        uiManager.totalPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
-        uiManager.curPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
         Transform[] points = GameObject.Find("WarpPointGroup").GetComponentsInChildren<Transform>();
         if (points != null)
         {
