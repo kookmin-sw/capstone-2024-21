ee002d634c7450b4b1a9e746ed8f476c629c9793
Noewewwwew <dnjs4475@naver.com>
2024-05-22 17:55:35 +0900

    모두가 죽는 버그 해결

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -97,7 +97,6 @@ public class GameManager : MonoBehaviour
         uiManager = GameObject.FindObjectOfType<UIManager>();
         isPlaying = false;
         uiManager.isGameOver = true;
-        AllDie();
     }
 
     public void Go2Map()
